2ce9b3f4a52ec2794936df19c679a89f9457a4da
parkcitymedia <blakemichaelgaynor@gmail.com>
2021-11-02 23:00:04 -0700

    clear clutter in duo.rs hope we didn't need that

--- src/duo.rs
+++ src/duo.rs
@@ -1,25 +1,5 @@
 use crate::{config::Config, *};
 
-/// login takes in a username, password, a url, and a mutable
-/// reference to a client and sends out a mutated/tokenized client
-/*
-pub async fn login(
-    login_username: &String,
-    login_password: &String,
-    login_url: &str,
-    login_client: &mut Client,
-) -> Result<Client, Box<dyn std::error::Error>> {
-    println!("{}{}", &login_url, &login_username);
-    let resp = login_client
-        .get(&(format!("{}{}",&login_url, &login_username)))
-        .text()
-        .await?;
-
-    println!("response: {:#?}", resp);
-    Ok(login_client.clone())
-}
-*/
-
 /// fetches duolingo data for you and tracked users
 pub async fn fetch(config: &Config,url: &str) -> Result<String, Box<dyn std::error::Error>> {
     //unimplemented!();
