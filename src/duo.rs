636d61e913fbf337edd0f29be5bb438119ee899a
parkcitymedia <blakemichaelgaynor@gmail.com>
2021-11-18 14:38:28 -0800

    ***BROKEN*** begin to unfuck the client-auth situation.

--- src/duo.rs
+++ src/duo.rs
@@ -1,43 +1,103 @@
 use crate::{config::Config, *};
-use reqwest::Response;
+//use serde::Serialize;
+//use serde_json::{Value, to_string, to_value};
+use reqwest::{Response, header::HeaderMap};
 use std::collections::{HashMap};
+use regex::Regex;
 
 pub async fn login(username: &String,password: &String, endpoint: &str) -> Result<Client, Box<dyn std::error::Error>> {
+
+    // Insert relevant headers
+    let content_type = String::from("application/json");
+    let accept = String::from("text/plain");
+    let accept_encoding = String::from("identity");
+    let user_agent = String::from("duoalert_oxide");
+
+    let mut login_json = HashMap::new();
+    let mut login_headers = HeaderMap::new();
+
+    println!("Inserting headers");
+    login_json.insert("login",username);
+    login_json.insert("password",password);
     
-    let mut session: Client = Client::new();
+    login_headers.insert("Content-Type", (&content_type).parse()?);
+    login_headers.insert("Accept",(&accept).parse()?);
+    login_headers.insert("Accept-Encoding",(&accept_encoding).parse()?);
+    login_headers.insert("User-Agent",(&user_agent).parse()?);
+    
+    println!("done.\n");
+
+    let client = Client::builder().default_headers(login_headers.clone()).build()?;
+
+    println!("Posting auth request");
+    let resp = client
+        .post(endpoint)
+        .json(&login_json)
+        .send().await?;
+    println!("done.\n");
+
+    // make that shit USABLE
+    println!("converting response to string");
+    let resp_string_csrf = &(format!("{:#?}",&resp)[1269..1329]);
+
+    println!("\n\n{:#?}\n\n",&resp_string_csrf);
+
+    let resp_string_jwt = &(format!("{:#?}",&resp)[1703..1840]);
+    
+    //let resp_vec: &Vec<char> = &(resp_string.chars().collect::<Vec<char>>());
+    //println!("{:?}",&resp_string);
+    let strip_mangles = Regex::new("\"")?;
+    println!("jwt:");
+    let jwt_string = format!("Bearer {:#?}",&resp_string_jwt);
+
+    let jwt_string = String::from(strip_mangles.replace_all(&jwt_string,""));
+    println!("done.\n");
+    println!("{}",&jwt_string);
+
+    println!("inserting jwt");
+    //login_headers.insert("Authorization",(&jwt_string).parse()?);
+
+    println!("\n{:?}",&login_json);
+
+    // return the client eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjYzMDcyMDAwMDAsImlhdCI6MCwic3ViIjoyNzgwMjI0MDd9.prezuFQ2Uq_CDyHVXtukHjn-t6q5EjcmSoSXqXUqhbI
+    let mut auth_map = HeaderMap::new();
+    auth_map.insert("Authorization",(&jwt_string).parse()?);
+    auth_map.insert("Content-Type", (&content_type).parse()?);
+    auth_map.insert("Accept",(&accept).parse()?);
+    auth_map.insert("Accept-Encoding",(&accept_encoding).parse()?);
+    auth_map.insert("User-Agent",(&user_agent).parse()?);
+
+    let jwt_client = Client::builder().default_headers(auth_map).build()?;
+
+    println!("{:#?}",&jwt_client);
+    Ok(jwt_client)
+}
+
+/// fetches duolingo data for you and tracked users
+pub async fn fetch(config: &Config,client: &mut Client) -> Result<String, Box<dyn std::error::Error>> {
+    
+    let main_fetch_url = format!("https://duolingo.com/users/{}",&config.username);
+
+    /*
+    let mut login_json = HashMap::new();
 
     let content_type = String::from("application/json");
     let accept = String::from("text/plain");
     let accept_encoding = String::from("identity");
     let user_agent = String::from("duoalert_oxide");
 
-    // Insert headers
-    let mut login_json = HashMap::new();
-    login_json.insert("login",username);
-    login_json.insert("password",password);
+    println!("Inserting headers");
+    login_json.insert("login",&config.username);
+    login_json.insert("password",&config.password);
     login_json.insert("Content-Type", &content_type);
     login_json.insert("Accept",&accept);
     login_json.insert("Accept-Encoding",&accept_encoding);
     login_json.insert("User-Agent",&user_agent);
+    println!("done.\n");
+    */
 
-    let resp = session
-    .post(endpoint)
-    .json(&login_json)
-    .send().await;
-    println!("{:?}",resp);
-    
-    // return the session
-    Ok(session)
-}
-
-/// fetches duolingo data for you and tracked users
-pub async fn fetch(config: &Config,session: &mut Client) -> Result<String, Box<dyn std::error::Error>> {
-    
-    let main_fetch_url = format!("https://duolingo.com/users/{}",&config.username);
-
-    let resp  = session
+    let resp  = client
     .get(main_fetch_url)
-    //.header("Authorization")
     .send().await?
     .text().await?;
 
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use std::{
-    fs::{File,read_to_string},
-    path::{Path},
-    collections::{HashMap}
+    collections::HashMap,
+    fs::{read_to_string, File},
+    path::Path,
 };
 
-use reqwest::{Client,get};
+use reqwest::{Client};
 //use serde::{Deserialize,Serialize};
-use serde_json::{Value};
+use serde_json::Value;
 
 pub mod config;
 pub mod discord;
 pub mod duo;
 use duo::fetch;
 pub mod duo_data;
-use duo_data::{update,check};
-
+use duo_data::{check, update};
 
 /// MAIN. RUNS FIRST
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     // define file paths
     let config_path: &str = "config.json";
     let streak_data_path: &str = "streak_data.json";
@@ -29,33 +27,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // check if config path exists
     if !Path::new(config_path).exists() {
-        
         // cry about nonexistent path
         println!("no data");
-
     } else {
-
         // login with stored details
         let my_config = config::get_config(config_path).await?;
-        let mut session = duo::login(
-            &my_config.username,
-            &my_config.password,
-            &login_endpoint
-        ).await?;
+        let mut session =
+            duo::login(&my_config.username, &my_config.password, &login_endpoint).await?;
 
+        println!("fetching userdata...");
         fetch(&my_config,&mut session).await?;
 
         // check if streak data exists
         if !Path::new(streak_data_path).exists() {
-
             // if not, cry about nonexistent path
             println!("no data");
-            
         } else {
 
             // if so, check the data in the file
             //check();
-
         };
 
         //update();
