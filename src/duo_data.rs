9d8dc32a32f8aa44b4926ec086e1359f3ae2569a
parkcitymedia <blakemichaelgaynor@gmail.com>
2022-01-12 21:31:24 -0800

    remove empty mod `duo_data`

--- src/duo_data.rs
+++
@@ -1,9 +0,0 @@
-
-
-/*
-pub async fn mine_jwt(resp: Response) -> Result<String, Box<dyn std::error::Error>> {
-    unimplemented!();
-    //let json_string: String = resp.json().await?.unwrap().text();
-
-    //Ok(json_string)
-}*/
